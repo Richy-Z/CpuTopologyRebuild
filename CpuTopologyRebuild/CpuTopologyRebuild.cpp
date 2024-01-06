00d8c70b8d210cb189c7554b99a87c9bbf5ceada
Richy Z <64844585+Richy-Z@users.noreply.github.com>
2024-01-06 02:25:52 +0000

    CPU Cluster Support

--- CpuTopologyRebuild/CpuTopologyRebuild.cpp
+++ CpuTopologyRebuild/CpuTopologyRebuild.cpp
@@ -3,22 +3,26 @@
 
 #include "CpuTopologyRebuild.h"
 #include <i386/cpu_topology.h>
-// #include <i386/cpuid.h>
 #include <Headers/kern_api.hpp>
 #include <Headers/kern_patcher.hpp>
 #include <Headers/kern_version.hpp>
 
-OSDefineMetaClassAndStructors(CpuTopologyRebuild, IOService)
+// Define a structure to represent a cluster
+struct Cluster {
+    x86_lcpu_t *primaryCore;
+    x86_lcpu_t *hyperThreadCores[P_CORE_MAX_COUNT]; // Assuming a maximum of P_CORE_MAX_COUNT hyper-thread cores
+    int numHyperThreadCores;
+};
+
+OSDefineMetaClassAndStructors(CpuTopologyRebuild, IOService);
 
 bool ADDPR(debugEnabled) = true;
 uint32_t ADDPR(debugPrintDelay) = 0;
 
 bool smt_spoof = false;
-x86_lcpu_t *p0_cpus[P_CORE_MAX_COUNT]; // P-Cores
-x86_lcpu_t *p1_cpus[P_CORE_MAX_COUNT]; // P-Cores HT
+Cluster pClusters[P_CORE_MAX_COUNT]; // P-Core clusters
 x86_lcpu_t *e0_cpus[E_CORE_MAX_COUNT]; // E-Cores
-int p0_count, p1_count, e0_count;
-int e_core_first = -1;
+int pClusterCount, e0_count;
 
 extern "C" void x86_validate_topology(void);
 
@@ -101,33 +105,40 @@ static void print_cpu_topology(void) {
 static void load_cpus(void) {
     x86_pkg_t  *pkg = x86_pkgs;
     x86_lcpu_t *cpu;
-    x86_lcpu_t *cpus_reverse[P_CORE_MAX_COUNT * 2 + E_CORE_MAX_COUNT];
-    int count = 0;
+    int e_core_first = -1;
 
-    p0_count = p1_count = e0_count = 0;
+    pClusterCount = e0_count = 0;
 
-    cpu = pkg->lcpus;
-    while (cpu != nullptr) {
-        cpus_reverse[count++] = cpu;
-        if (e_core_first == -1 || e_core_first - cpu->pnum == 2) {
-            e_core_first = cpu->pnum;
-        }
-        cpu = cpu->next_in_pkg;
-    }
-    for (int i=0; i<count; ++i) {
-        cpu = cpus_reverse[count - 1 - i];
-        if (cpu->pnum < e_core_first) { // P-Core
-            if (cpu->pnum % 2 == 0) { // primary
-                p0_cpus[p0_count++] = cpu;
-            } else { // HT core
-                p1_cpus[p1_count++] = cpu;
+    // Traverse through the CPU packages
+    while (pkg != nullptr) {
+        cpu = pkg->lcpus;
+
+        // Create a new P-Core cluster
+        pClusters[pClusterCount].primaryCore = cpu;
+        pClusters[pClusterCount].numHyperThreadCores = 0;
+
+        while (cpu != nullptr) {
+            if (e_core_first == -1 || e_core_first - cpu->pnum == 2) {
+                e_core_first = cpu->pnum;
             }
-        } else { // E-Core
-            e0_cpus[e0_count++] = cpu;
+
+            // Check if the core is a hyper-thread core
+            if (cpu->pnum % 2 != 0) {
+                pClusters[pClusterCount].hyperThreadCores[pClusters[pClusterCount].numHyperThreadCores++] = cpu;
+            }
+
+            cpu = cpu->next_in_pkg;
         }
+
+        // Move to the next package
+        pkg = pkg->next;
+        pClusterCount++;
     }
 
-    if (p1_count == 0) {
+    // Set the E-Core count
+    e0_count = e_core_first != -1 ? (pClusterCount * 2) : 0;
+
+    if (pClusters[pClusterCount - 1].numHyperThreadCores == 0) {
         smt_spoof = true;
     }
 }
@@ -140,13 +151,13 @@ static void rebuild_cache_topology(void) {
     // E-Core fix
     x86_lcpu_t *e_primary;
 
-    for (int i=0; i<(e0_count/4); ++i) {
-        e_primary = e0_cpus[i*4];
+    for (int i = 0; i < (e0_count / 4); ++i) {
+        e_primary = e0_cpus[i * 4];
         e_primary->caches[0]->cache_size = 64 * 1024; // 64KB
         l2 = e_primary->caches[1];
         l2->cache_size = 2 * 1024 * 1024; // 2MB
-        for (int j=1; j<4; ++j) {
-            cpu = e0_cpus[i*4+j];
+        for (int j = 1; j < 4; ++j) {
+            cpu = e0_cpus[i * 4 + j];
             cpu->caches[0]->cache_size = 64 * 1024; // 64KB
             cpu->caches[1] = l2;
             l2->cpus[j] = cpu;
@@ -158,17 +169,22 @@ static void rebuild_cache_topology(void) {
     x86_lcpu_t *p0;
     x86_lcpu_t *p1;
 
-    for (int i=0; i<p1_count; ++i) {
-        p0 = p0_cpus[i];
-        p1 = p1_cpus[i];
+    for (int i = 0; i < pClusterCount; ++i) {
+        p0 = pClusters[i].primaryCore;
         l1 = p0->caches[0];
         l2 = p0->caches[1];
 
-        p1->caches[0] = l1;
-        p1->caches[1] = l2;
+        for (int j = 0; j < pClusters[i].numHyperThreadCores; ++j) {
+            p1 = pClusters[i].hyperThreadCores[j];
+            p1->caches[0] = l1;
+            p1->caches[1] = l2;
 
-        l1->nlcpus = l2->nlcpus = 2;
-        l1->cpus[1] = l2->cpus[1] = p1;
+            l1->nlcpus++;
+            l2->nlcpus++;
+
+            l1->cpus[l1->nlcpus - 1] = p1;
+            l2->cpus[l2->nlcpus - 1] = p1;
+        }
     }
 }
 
@@ -181,56 +197,56 @@ static void rebuild_cpu_topology(void) {
     x86_core_t *core;
     x86_lcpu_t *cpu;
 
-    // i386_cpu_info_t *info = cpuid_info();
-    x86_core_t *p_core_last = p0_cpus[p0_count - 1]->core;
+    x86_core_t *p_core_last = pClusters[pClusterCount - 1].primaryCore->core;
     if (smt_spoof) {
         pkg->cores = die->cores = p_core_last;
-        die->num_cores = p0_count;
-        machine_info.physical_cpu_max = p0_count;
-        // info->core_count = p0_count;
+        die->num_cores = pClusterCount;
+        machine_info.physical_cpu_max = pClusterCount;
     } else {
         core = e0_cpus[0]->core;
         core->next_in_die = core->next_in_pkg = p_core_last;
-        die->num_cores = p0_count + e0_count;
-        machine_info.physical_cpu_max = p0_count + e0_count;
-        // info->core_count = p0_count + e0_count;
+        die->num_cores = pClusterCount + e0_count;
+        machine_info.physical_cpu_max = pClusterCount + e0_count;
     }
 
-    for (int i=0; i<p0_count; ++i) {
-        cpu = p0_cpus[i];
-        cpu->lnum = 0;
-        core = cpu->core;
-        core->lcore_num = core->pcore_num = i;
-        core->num_lcpus = 1;
-        core->lcpus = cpu;
-        if (i != 0) {
-            core->next_in_pkg = core->next_in_die = p0_cpus[i-1]->core;
+    int pClusterIndex = 0;
+
+    while (pkg != nullptr) {
+        // Set the core information for P-Core clusters
+        for (int i = 0; i < pClusters[pClusterIndex].numHyperThreadCores; ++i) {
+            cpu = pClusters[pClusterIndex].hyperThreadCores[i];
+            core = pClusters[pClusterIndex].primaryCore->core;
+
+            cpu->lnum = core->num_lcpus++;
+            cpu->core = core;
+            cpu->next_in_core = core->lcpus;
+            core->lcpus = cpu;
         }
+
+        // Move to the next package
+        pkg = pkg->next;
+        pClusterIndex++;
     }
-    for (int i=0; i<p1_count; ++i) {
-        cpu = p1_cpus[i];
-        core = p0_cpus[i]->core;
-        cpu->lnum = core->num_lcpus++;
-        cpu->core = core;
-        cpu->next_in_core = core->lcpus;
-        core->lcpus = cpu;
-    }
-    for (int i=0; i<e0_count; ++i) {
+
+    for (int i = 0; i < e0_count; ++i) {
         cpu = e0_cpus[i];
         if (smt_spoof) {
-            core = p0_cpus[i % p0_count]->core;
+            // Set the core information for E-Cores in SMT spoof mode
+            core = pClusters[i % pClusterCount].primaryCore->core;
             cpu->core = core;
             cpu->lnum = core->num_lcpus++;
             cpu->next_in_core = core->lcpus;
             core->lcpus = cpu;
         } else {
-            cpu->core->lcore_num = cpu->core->pcore_num = p0_count + i;
+            // Set the core information for E-Cores
+            cpu->core->lcore_num = cpu->core->pcore_num = pClusterCount + i;
         }
     }
 
     rebuild_cache_topology();
 }
 
+// Update the my_x86_validate_topology function
 void my_x86_validate_topology(void) {
     load_cpus();
     DBGLOG("ctr", "---- CPU/Cache topology before rebuild ----");
